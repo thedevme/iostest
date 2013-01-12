9988a853a0341766030a8f974f8a42b05844a6c7
thedevme <craig@thedev.me>
2013-01-12 14:28:08 -0500

    Initial Commit

---
+++ IntoStoryboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IntoStoryboard
+//
+//  Created by Craig Clayton on 1/12/13.
+//  Copyright (c) 2013 Craig Clayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
